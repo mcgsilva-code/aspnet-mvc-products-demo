8e101d5fabe20002781770a54bfe8303136efe2b
Cristiane <mcrisgs09@gmail.com>
2026-01-16 17:13:31 +0100

    Commit iniziali:ProductsController e List View in ASP .NET MVC

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetWebApp.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetWebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
